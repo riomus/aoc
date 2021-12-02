d694a97c164e5bd52952bfef5e00ab0e41890acd
Roman Bartusiak <roman@stashaway.com>
2021-12-02 08:03:46 +0100

    feat: t2

---
+++ lib/src/main/kotlin/ml/bartusiak/aoc/2021/T21.kt
@@ -0,0 +1,23 @@
+package ml.bartusiak.aoc.`2021`
+
+import ml.bartusiak.aoc.AOCTask
+
+object T21: AOCTask{
+
+    data class Line(val direction: String, val distance: Int)
+
+    fun solve(): Int {
+        val (depth, horizontal) = data("T2.txt").map{
+            val data = it.split(" ")
+            Line(data[0], data[1].toInt())
+        }.fold(Pair(0, 0)) { acc, line ->
+            when (line.direction) {
+                "forward" -> acc.copy(second = acc.second + line.distance)
+                "up" -> acc.copy(first = acc.first - line.distance)
+                else -> acc.copy(first = acc.first + line.distance)
+            }
+        }
+        return depth*horizontal
+    }
+
+}
---
+++ lib/src/test/kotlin/ml/bartusiak/aoc/2021/T2Test.kt
@@ -0,0 +1,16 @@
+package ml.bartusiak.aoc.`2021`
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class T2Test: StringSpec({
+
+    "T21 should return correct result" {
+        T21.solve() shouldBe 1698735
+    }
+
+    "T22 should return correct result" {
+        T22.solve() shouldBe 1594785890
+    }
+
+})
