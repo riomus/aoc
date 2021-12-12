13993517a08e5f1dde7fae58f2d19249a19ce9cf
Roman Bartusiak <roman@stashaway.com>
2021-12-12 19:21:04 +0100

    feat: t12

---
+++ lib/src/main/kotlin/ml/bartusiak/aoc/2021/T122.kt
@@ -0,0 +1,21 @@
+package ml.bartusiak.aoc.`2021`
+
+import ml.bartusiak.aoc.AOCTask
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
+import kotlin.math.sign
+
+import kotlin.reflect.KFunction1
+open class T122: T121(){
+
+    companion object {
+        fun solve(file: String = "T12.txt"): Int = T122().solve(file, true)
+    }
+
+
+}
+
+
+
+
---
+++ lib/src/test/kotlin/ml/bartusiak/aoc/2021/T12Test.kt
@@ -0,0 +1,27 @@
+package ml.bartusiak.aoc.`2021`
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class T12Test: StringSpec({
+
+    "T121 should return correct result for test data" {
+        T121.solve("T12_test.txt") shouldBe 10
+    }
+    "T121 should return correct result for large test data" {
+        T121.solve("T12_test_large.txt") shouldBe 19
+    }
+    "T121 should return correct result" {
+        T121.solve("T12.txt") shouldBe 3563
+    }
+    "T122 should return correct result for test data" {
+        T122.solve("T12_test.txt") shouldBe 36
+    }
+    "T122 should return correct result for large test data" {
+        T122.solve("T12_test_large.txt") shouldBe 103
+    }
+    "T122 should return correct result" {
+        T122.solve("T12.txt") shouldBe 105453
+    }
+
+})
